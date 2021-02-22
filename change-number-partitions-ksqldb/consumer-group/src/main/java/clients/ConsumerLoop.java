60a1b923c2ebe33cb5aacb5423ebc29eee31ec02
Borja Hernandez Crespo <41290263+borjahernandez@users.noreply.github.com>
2021-02-22 19:18:33 +0000

    Add change-number-partitions-ksqldb Demo

---
+++ change-number-partitions-ksqldb/consumer-group/src/main/java/clients/ConsumerLoop.java
@@ -0,0 +1,62 @@
+package clients;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class ConsumerLoop implements Runnable {
+    private final KafkaConsumer<String, String> consumer;
+    private final List<String> topics;
+    private final int id;
+
+    public ConsumerLoop(int id, String groupId, List<String> topics, String autoOffsetReset, Properties settings) {
+        this.id = id;
+        this.topics = topics;
+        settings.put(ConsumerConfig.CLIENT_ID_CONFIG, ("Consumer" + id));
+        settings.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        settings.put("ssl.endpoint.identification.algorithm", "https");
+        settings.put("security.protocol", "SASL_SSL");
+        settings.put("sasl.mechanism", "PLAIN");
+
+        this.consumer = new KafkaConsumer<>(settings);
+    }
+
+    @Override
+    public void run() {
+        try {
+            consumer.subscribe(topics);
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("partition", record.partition());
+                    data.put("offset", record.offset());
+                    data.put("value", record.value());
+                    System.out.println("Consumer-" + this.id + ": " + data);
+                }
+            }
+        } catch (WakeupException e) {
+            // ignore for shutdown
+        } finally {
+            consumer.close();
+        }
+    }
+
+    public void shutdown() {
+        consumer.wakeup();
+    }
+
+}
